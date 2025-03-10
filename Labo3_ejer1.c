6e874ff249d9a127fa16f634a929c98151a92981
MaGabRam <136017727+MaGabRam@users.noreply.github.com>
2025-03-10 16:50:58 -0400

    Update Labo3_ejer1.c

--- Labo3_ejer1.c
+++ Labo3_ejer1.c
@@ -26,38 +26,38 @@ __error__(char *pcFilename, uint32_t ui32Line)
 int
 main(void)
 {
-    // Configura el reloj del sistema a 120 MHz
+    
     SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
 
-    // Habilita el periférico GPIO para el puerto N
+    
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
 
-    // Espera a que el periférico esté listo
+   
     while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
     {
     }
 
-    // Configura los pines PN0 y PN1 como salidas
+    // pines PN0 y PN1 como salidas
     GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     while(1)
     {
-        // Enciende el LED en PN0
+      
         GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
         
-        // Retardo de 1 segundo (aproximadamente)
+        
         SysCtlDelay(120000000 / 3);
         
-        // Enciende el LED en PN1
+        
         GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1);
         
-        // Retardo de 1 segundo (aproximadamente)
+        
         SysCtlDelay(120000000 / 3);
         
-        // Apaga ambos LEDs
+        
         GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1, 0x00);
         
-        // Retardo de 1 segundo (aproximadamente)
+       
         SysCtlDelay(120000000 / 3);
     }
     
